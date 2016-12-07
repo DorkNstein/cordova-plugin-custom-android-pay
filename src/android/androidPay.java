1fb7eee1e79c9772e562f0803f6087ce0a61abdb
Yeshwanth Malekar <yeshwanth.m@seventablets.com>
2016-12-07 11:26:20 -0600

    back to one file style

--- src/android/androidPay.java
+++ src/android/androidPay.java
@@ -8,6 +8,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.google.android.gms.wallet.Cart;
 import com.google.android.gms.wallet.LineItem;
@@ -29,6 +30,9 @@ import java.util.List;
 public class androidPay extends CordovaPlugin {
 
     private String mEnv;
+    private GoogleApiClient mGoogleApiClient;
+    public static final int FULL_WALLET_REQUEST_CODE = 889;
+    private FullWallet mFullWallet;
 
     @Override
     public boolean execute(String action, JSONArray data, CallbackContext callbackContext) throws JSONException {
@@ -38,6 +42,15 @@ public class androidPay extends CordovaPlugin {
             String name = data.getString(0);
             String message = "Hello, " + name;
             // createMaskedWalletRequest("0.01", callbackContext);
+            MaskedWalletRequest maskedWalletRequest = createMaskedWalletRequest(amountText);
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .enableAutoManage(this, 0, this)
+                .addApi(Wallet.API, new Wallet.WalletOptions.Builder()
+                    .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
+                    .setTheme(WalletConstants.THEME_LIGHT)
+                    .build())
+                .build();
             callbackContext.success(message);
 
             return true;
@@ -47,6 +60,114 @@ public class androidPay extends CordovaPlugin {
             return false;
 
         }
+    }
+
+    private MaskedWalletRequest generateMaskedWalletRequest() {
+        // This is just an example publicKey for the purpose of this codelab. 
+        // To learn how to generate your own visit:
+        // https://github.com/android-pay/androidpay-quickstart
+        String publicKey = "BO39Rh43UGXMQy5PAWWe7UGWd2a9YRjNLPEEVe+zWIbdIgALcDcnYCuHbmrrzl7h8FZjl6RCzoi5/cDrqXNRVSo=";
+        PaymentMethodTokenizationParameters parameters =
+                PaymentMethodTokenizationParameters.newBuilder()
+                        .setPaymentMethodTokenizationType(
+                             PaymentMethodTokenizationType.NETWORK_TOKEN)
+                        .addParameter("publicKey", publicKey)
+                        .build();
+
+        MaskedWalletRequest maskedWalletRequest =
+                MaskedWalletRequest.newBuilder()
+                        .setMerchantName("Google I/O Codelab")
+                        .setPhoneNumberRequired(true)
+                        .setShippingAddressRequired(true)
+                        .setCurrencyCode("USD")
+                        .setCart(Cart.newBuilder()
+                                .setCurrencyCode("USD")
+                                .setTotalPrice("10.00")
+                                .addLineItem(LineItem.newBuilder()
+                                        .setCurrencyCode("USD")
+                                        .setDescription("Google I/O Sticker")
+                                        .setQuantity("1")
+                                        .setUnitPrice("10.00")
+                                        .setTotalPrice("10.00")
+                                        .build())
+                                .build())
+                        .setEstimatedTotalPrice("15.00")
+                        .setPaymentMethodTokenizationParameters(parameters)
+                        .build();
+        return maskedWalletRequest;
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case MASKED_WALLET_REQUEST_CODE:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        mMaskedWallet =  data
+                                .getParcelableExtra(WalletConstants.EXTRA_MASKED_WALLET);
+                        Toast.makeText(this, "Got Masked Wallet", Toast.LENGTH_SHORT).show();
+                        break;
+                    case RESULT_CANCELED:
+                        // The user canceled the operation
+                        break;
+                    case WalletConstants.RESULT_ERROR:
+                        Toast.makeText(this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+                        callbackContext.error("An Error Occurred!");
+                        break;
+                }
+                break;
+            case FULL_WALLET_REQUEST_CODE:
+                switch (resultCode) {
+                    case RESULT_OK:
+                        mFullWallet = data
+                                .getParcelableExtra(WalletConstants.EXTRA_FULL_WALLET);
+                        // Show the credit card number
+                        Toast.makeText(this,
+                                "Got Full Wallet, Done!",
+                                Toast.LENGTH_SHORT).show();
+                        callbackContext.success(mFullWallet.getPaymentMethodToken());
+                        break;
+                    case WalletConstants.RESULT_ERROR:
+                        Toast.makeText(this, "An Error Occurred", Toast.LENGTH_SHORT).show();
+                        callbackContext.error("An Error Occurred during Full Wallet Request!");
+                        break;
+                }
+                break;    
+        }
+    }
+
+    private FullWalletRequest generateFullWalletRequest(String googleTransactionId) {
+      FullWalletRequest fullWalletRequest = FullWalletRequest.newBuilder()
+          .setGoogleTransactionId(googleTransactionId)
+          .setCart(Cart.newBuilder()
+              .setCurrencyCode("USD")
+              .setTotalPrice("10.10")
+              .addLineItem(LineItem.newBuilder()
+                  .setCurrencyCode("USD")
+                  .setDescription("Google I/O Sticker")
+                  .setQuantity("1")
+                  .setUnitPrice("10.00")
+                  .setTotalPrice("10.00")
+                  .build())
+              .addLineItem(LineItem.newBuilder()
+                  .setCurrencyCode("USD")
+                  .setDescription("Tax")
+                  .setRole(LineItem.Role.TAX)
+                  .setTotalPrice(".10")
+                  .build())
+              .build())
+          .build();
+      return fullWalletRequest;
+    }
+
+    public void requestFullWallet(View view) {
+      if (mMaskedWallet == null) {
+        Toast.makeText(this, "No masked wallet, can't confirm", Toast.LENGTH_SHORT).show();
+        return;
+      } 
+      Wallet.Payments.loadFullWallet(mGoogleApiClient,
+          generateFullWalletRequest(mMaskedWallet.getGoogleTransactionId()),
+          FULL_WALLET_REQUEST_CODE);
     }
 }
 
